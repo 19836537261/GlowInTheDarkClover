97e57fa8d973c36ae0967e7b0440390b4c421353
荧光百叶草 <1295284735@qq.com>
2026-03-16 11:44:27 +0800

    Just a strip of animation

---
+++ src/main/java/net/etechservicecn/glow_in_the_dark_clover/teleporters/FireWorldTeleporter.java
@@ -0,0 +1,7 @@
+package net.etechservicecn.glow_in_the_dark_clover.teleporters;
+
+import net.minecraftforge.common.util.ITeleporter;
+
+public class FireWorldTeleporter implements ITeleporter {
+
+}
